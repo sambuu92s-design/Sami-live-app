d41b2fb47e5bd83a9567fcbf158924170a980256
sambuu92s-design <sambuu.92s@gmail.com>
2026-09-06 07:19:21 +0930

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,26 @@
+importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCqRq6A_IopzQHNuXOczeYNWENhjHj3AAY",
+  authDomain: "sami-app-project.firebaseapp.com",
+  projectId: "sami-app-project",
+  storageBucket: "sami-app-project.firebasestorage.app",
+  messagingSenderId: "417143307752",
+  appId: "1:417143307752:web:00a868aad4cf6bb2b8b85d"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const title = payload.notification?.title || "Shift App";
+
+  const options = {
+    body: payload.notification?.body || "You have a new shift notification."
+  };
+
+  self.registration.showNotification(title, options);
+});
 const CACHE = "shift-app-shell-v1";
 const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest"];
 
